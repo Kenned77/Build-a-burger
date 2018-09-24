7772c196344cb983b9e509b0c1a1c0bceec96f1a
Kenneth Bidstrup <kenneth.bidstrup@gmail.com>
2018-09-24 14:47:39 -0700

    Add addIngredientHandler to add ingredients dynamically

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,13 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.5,
+  meat: 1.3,
+  bacon: 0.7
+}
+
 class BurgerBuilder extends Component {
 
   state = {
@@ -11,7 +18,26 @@ class BurgerBuilder extends Component {
       bacon: 1,
       cheese: 2,
       meat: 2
-    }
+    },
+    totalPrice: 4
+  }
+
+  addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = updatedCount;
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+    this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+
+  }
+
+  removeIngredientHandler = (type) => {
+
   }
 
   render() {
@@ -19,7 +45,8 @@ class BurgerBuilder extends Component {
       <div>
         <Aux>
           <Burger ingredients={this.state.ingredients}/>
-          <BuildControls />
+          <BuildControls
+            ingredientsAdded={this.addIngredientHandler}/>
         </Aux>
       </div>
     );
