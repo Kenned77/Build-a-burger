7e7406e929d9198f797497bf12df2a54a4314c24
Kenneth Bidstrup <kenneth.bidstrup@gmail.com>
2018-09-24 17:48:47 -0700

    Add button component and use in summary component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Aux from '../../../hoc/Aux';
+import Button from '../../UI/Button/Button'
 
 const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients)
@@ -14,12 +15,16 @@ const orderSummary = (props) => {
     <Aux>
       <h3>Your Order</h3>
       <p>A delicious burger with the following ingredients:</p>
+      <h1>Too much meat Sean Linke!!!</h1>
       <ul>
         {ingredientSummary}
       </ul>
       <p>Continue to checkout?</p>
+      <Button btnType="Danger" clicked={props.purchaseCancled}>CANCEL</Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
     </Aux>
   );
 };
 
 export default orderSummary;
+
